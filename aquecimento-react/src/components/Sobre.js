c9b8a3068f3528bf70c4ca0ee593160b7c1ee788
CelestePrimaz <celesteprimaz@gmail.com>
2019-08-08 19:12:40 -0400

    Roteamento de pagina

---
+++ aquecimento-react/src/components/Sobre.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Sobre extends Component {
+    render() {
+        return (
+            <div>
+                Sobre
+            </div>
+        );
+    }
+}
+
